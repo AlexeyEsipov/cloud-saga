518454ab97bc3fc9f429e66327de84742764b44a
AlexeyEsipov <a_esipov@list.ru>
2025-10-04 20:47:46 +0300

    init commit

---
+++ src/main/java/ru/esipov/saga/config/KafkaConfig.java
@@ -0,0 +1,88 @@
+package ru.esipov.saga.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+public class KafkaConfig {
+
+    @Value("${orders.events.topic.name}")
+    private String ordersEventsTopicName;
+
+    @Value("${orders.commands.topic.name}")
+    private String ordersCommandsTopicName;
+
+    @Value("${products.commands.topic.name}")
+    private String productsCommandsTopicName;
+
+    @Value("${products.events.topic.name}")
+    String productsEventsTopicName;
+
+    @Value("${payments.command.topic.name}")
+    private String paymentsCommandsTopicName;
+
+    @Value("${payments.events.topic.name}")
+    private String paymentsEventsTopicName;
+
+    private static final Integer TOPIC_REPLICATION_FACTOR=3;
+    private static final Integer TOPIC_PARTITIONS=3;
+
+
+    @Bean
+    KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    NewTopic createOrdersEventsTopic() {
+        return TopicBuilder.name(ordersEventsTopicName)
+                .partitions(TOPIC_PARTITIONS)
+                .replicas(TOPIC_REPLICATION_FACTOR)
+                .build();
+    }
+
+    @Bean
+    NewTopic createPaymentsEventsTopic() {
+        return TopicBuilder.name(paymentsEventsTopicName)
+                .partitions(TOPIC_PARTITIONS)
+                .replicas(TOPIC_REPLICATION_FACTOR)
+                .build();
+    }
+
+    @Bean
+    NewTopic createProductsCommandsTopic() {
+        return TopicBuilder.name(productsCommandsTopicName)
+                .partitions(TOPIC_PARTITIONS)
+                .replicas(TOPIC_REPLICATION_FACTOR)
+                .build();
+    }
+
+    @Bean
+    NewTopic createProductsEventsTopic() {
+        return TopicBuilder.name(productsEventsTopicName)
+                .partitions(TOPIC_PARTITIONS)
+                .replicas(TOPIC_REPLICATION_FACTOR)
+                .build();
+    }
+
+    @Bean
+    NewTopic createPaymentsCommandsTopic() {
+        return TopicBuilder.name(paymentsCommandsTopicName)
+                .partitions(TOPIC_PARTITIONS)
+                .replicas(TOPIC_REPLICATION_FACTOR)
+                .build();
+    }
+
+    @Bean
+    NewTopic createOrdersCommandsTopic() {
+        return TopicBuilder.name(ordersCommandsTopicName)
+                .partitions(TOPIC_PARTITIONS)
+                .replicas(TOPIC_REPLICATION_FACTOR)
+                .build();
+    }
+}
---
+++ src/main/java/ru/esipov/saga/saga/Saga.java
@@ -0,0 +1,94 @@
+package ru.esipov.saga.saga;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+import ru.job4j.core.command.*;
+import ru.job4j.core.events.*;
+import ru.job4j.core.sagadto.OrderStatus;
+
+@Component
+@KafkaListener(topics = {
+        "${orders.events.topic.name}",
+        "${products.events.topic.name}",
+        "${payments.events.topic.name}"
+})
+public class Saga {
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final String productsCommandsTopicName;
+    private final String paymentsCommandsTopicName;
+    private final String ordersCommandsTopicName;
+
+    public Saga(KafkaTemplate<String, Object> kafkaTemplate,
+                @Value("${products.commands.topic.name}") String productsCommandsTopicName,
+                @Value("${payments.command.topic.name}") String paymentsCommandsTopicName,
+                @Value("${orders.commands.topic.name}") String ordersCommandsTopicName) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.productsCommandsTopicName = productsCommandsTopicName;
+        this.paymentsCommandsTopicName = paymentsCommandsTopicName;
+        this.ordersCommandsTopicName = ordersCommandsTopicName;
+    }
+
+    @KafkaHandler
+    public void handleEvent(@Payload OrderCreatedEvent event) {
+        ReserveProductCommand command = new ReserveProductCommand(
+                event.productId(), event.productQuantity(), event.orderId()
+        );
+        kafkaTemplate.send(productsCommandsTopicName, command);
+        OrderHistoryCommand orderHistoryCommand = new OrderHistoryCommand(event.orderId(), OrderStatus.CREATED);
+        kafkaTemplate.send(ordersCommandsTopicName, orderHistoryCommand);
+    }
+
+    @KafkaHandler
+    public void handleEvent(@Payload ProductReservedEvent event) {
+        ProcessPaymentCommand processPaymentCommand = new ProcessPaymentCommand(
+                event.orderId(),
+                event.productId(),
+                event.productPrice(),
+                event.productQuantity()
+        );
+        kafkaTemplate.send(paymentsCommandsTopicName, processPaymentCommand);
+
+    }
+
+    @KafkaHandler
+    public void handleEvent(@Payload PaymentProcessedEvent event) {
+        ApproveOrderCommand approveOrderCommand = new ApproveOrderCommand(
+                event.orderId()
+        );
+        kafkaTemplate.send(ordersCommandsTopicName, approveOrderCommand);
+    }
+
+    @KafkaHandler
+    public void handleEvent(@Payload OrderApprovedEvent event) {
+        OrderHistoryCommand orderHistoryCommand = new OrderHistoryCommand(event.orderId(), OrderStatus.APPROVED);
+        kafkaTemplate.send(ordersCommandsTopicName, orderHistoryCommand);
+    }
+
+    @KafkaHandler
+    public void handleEvent(@Payload PaymentFailedEvent event) {
+        CancelProductReservationCommand cancelProductReservationCommand = new CancelProductReservationCommand(
+                event.orderId(),
+                event.productId(),
+                event.productQuantity()
+        );
+        kafkaTemplate.send(productsCommandsTopicName, cancelProductReservationCommand);
+    }
+
+    @KafkaHandler
+    public void handleEvent(@Payload ProductReservationCancelledEvent event) {
+        RejectOrderCommand rejectOrderCommand = new RejectOrderCommand(event.orderId());
+        kafkaTemplate.send(ordersCommandsTopicName, rejectOrderCommand);
+        OrderHistoryCommand orderHistoryCommand = new OrderHistoryCommand(event.orderId(), OrderStatus.REJECTED);
+        kafkaTemplate.send(ordersCommandsTopicName, orderHistoryCommand);
+    }
+
+    @KafkaHandler
+    public void handleEvent(@Payload ProductReservationFailedEvent event) {
+        OrderHistoryCommand orderHistoryCommand = new OrderHistoryCommand(event.orderId(), OrderStatus.REJECTED);
+        kafkaTemplate.send(ordersCommandsTopicName, orderHistoryCommand);
+    }
+}
